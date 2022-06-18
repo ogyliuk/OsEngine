ba00409456aee4a24702a24f3b4c1b8d33cb00de
Oleg Gyliuk <oleg_gyliuk@epam.com>
2022-06-19 00:26:38 +0300

    Fixed logger format.

--- project/OsEngine/OlegUtils.cs
+++ project/OsEngine/OlegUtils.cs
@@ -16,7 +16,7 @@ namespace OsEngine
         {
             string logFileName = String.Format("OLEG_LOG.txt");
             string logFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), logFileName);
-            string threadId = String.Format("THREAD = {0}; ", Thread.CurrentThread.ManagedThreadId);
+            string threadId = String.Format("THREAD = {0};", Thread.CurrentThread.ManagedThreadId);
             string logLine = String.Format("{0} : {1} : {2}", threadId, DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss.fff"), message);
             File.AppendAllLines(logFilePath, new[] { logLine });
         }
