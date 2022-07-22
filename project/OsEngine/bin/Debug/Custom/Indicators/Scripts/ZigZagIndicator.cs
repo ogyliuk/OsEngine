b8d4bbfdbd7103f8b406d7e32d8d2c6d4707be3f
Oleg Gyliuk <oleg_gyliuk@epam.com>
2022-07-22 19:29:28 +0300

    Started doing divergence bot.

---
+++ project/OsEngine/bin/Debug/Custom/Indicators/Scripts/ZigZagIndicator.cs
@@ -0,0 +1,202 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using OsEngine.Entity;
+using OsEngine.Indicators;
+
+namespace CustomIndicators.Scripts
+{
+    public class ZigZagIndicator : Aindicator
+    {
+        private IndicatorParameterInt _period;
+        private IndicatorDataSeries _seriesZigZagLinePointsOfAllCandles;
+        private IndicatorDataSeries _seriesPeakPricesAll;
+        private IndicatorDataSeries _seriesPeakPricesHigh;
+        private IndicatorDataSeries _seriesPeakPricesLow;
+
+        private int _trendDirection = 0;
+        private int _peakCandidateIndex = -1;
+        private decimal _peakCandidatePrice = 0;
+
+        public override void OnStateChange(IndicatorState state)
+        {
+            _period = CreateParameterInt("Length", 14);
+
+            _seriesPeakPricesAll = CreateSeries("ZigZag", Color.CornflowerBlue, IndicatorChartPaintType.Point, false);
+            _seriesPeakPricesAll.CanReBuildHistoricalValues = true;
+
+            _seriesZigZagLinePointsOfAllCandles = CreateSeries("ZigZagLine", Color.CornflowerBlue, IndicatorChartPaintType.Point, true);
+            _seriesZigZagLinePointsOfAllCandles.CanReBuildHistoricalValues = true;
+
+            _seriesPeakPricesHigh = CreateSeries("_seriesZigZagHighs", Color.GreenYellow, IndicatorChartPaintType.Point, false);
+            _seriesPeakPricesHigh.CanReBuildHistoricalValues = true;
+
+            _seriesPeakPricesLow = CreateSeries("_seriesZigZagLows", Color.Red, IndicatorChartPaintType.Point, false);
+            _seriesPeakPricesLow.CanReBuildHistoricalValues = true;
+        }
+
+        public override void OnProcess(List<Candle> candles, int index)
+        {
+            if (index < _period.ValueInt * 2)
+            {
+                _trendDirection = 0;
+                _peakCandidateIndex = -1;
+                _peakCandidatePrice = 0;
+                return;
+            }
+
+            bool upPeakCandidateFound = false;
+            bool downPeakCandidateFound = false;
+            bool betterUpPeakCandidateFound = false;
+            bool betterDownPeakCandidateFound = false;
+            bool trendUp = _trendDirection > 0;
+            bool trendDown = _trendDirection < 0;
+            decimal currentCandleHigh = candles[index].High;
+            decimal currentCandleLow = candles[index].Low;
+
+            if (_peakCandidatePrice == 0)
+            {
+                decimal currentCandleMiddle = currentCandleLow + (currentCandleHigh - currentCandleLow) / 2;
+                _peakCandidatePrice = currentCandleMiddle;
+            }
+
+            bool isSwingHigh = currentCandleHigh == GetExtremum(candles, _period.ValueInt, "High", index);
+            bool isSwingLow = currentCandleLow == GetExtremum(candles, _period.ValueInt, "Low", index);
+            if (!isSwingHigh && !isSwingLow)
+            {
+                return;
+            }
+
+            if (isSwingHigh)
+            {
+                if (trendUp)
+                {
+                    if (currentCandleHigh >= _peakCandidatePrice)
+                    {
+                        _peakCandidatePrice = currentCandleHigh;
+                        betterUpPeakCandidateFound = true;
+                    }
+                }
+                else
+                {
+                    _trendDirection = 1; // UP
+                    _peakCandidatePrice = currentCandleHigh;
+                    upPeakCandidateFound = true;
+                }
+            }
+            else if (isSwingLow)
+            {
+                if (trendDown)
+                {
+                    if (currentCandleLow <= _peakCandidatePrice)
+                    {
+                        _peakCandidatePrice = currentCandleLow;
+                        betterDownPeakCandidateFound = true;
+                    }
+                }
+                else
+                {
+                    _trendDirection = -1; // DOWN
+                    _peakCandidatePrice = currentCandleLow;
+                    downPeakCandidateFound = true;
+                }
+            }
+
+            if (upPeakCandidateFound || downPeakCandidateFound || betterUpPeakCandidateFound || betterDownPeakCandidateFound)
+            {
+                bool alreadyHavePeakCandidate = _peakCandidateIndex >= 0;
+                if (alreadyHavePeakCandidate)
+                {
+                    if (betterUpPeakCandidateFound)
+                    {
+                        _seriesPeakPricesAll.Values[_peakCandidateIndex] = 0;
+                        _seriesPeakPricesHigh.Values[_peakCandidateIndex] = 0;
+                    }
+                    else if (betterDownPeakCandidateFound)
+                    {
+                        _seriesPeakPricesAll.Values[_peakCandidateIndex] = 0;
+                        _seriesPeakPricesLow.Values[_peakCandidateIndex] = 0;
+                    }
+                }
+
+                if (upPeakCandidateFound || betterUpPeakCandidateFound)
+                {
+                    _seriesPeakPricesAll.Values[index] = _peakCandidatePrice;
+                    _seriesPeakPricesHigh.Values[index] = _peakCandidatePrice;
+                }
+                else if (downPeakCandidateFound || betterDownPeakCandidateFound)
+                {
+                    _seriesPeakPricesAll.Values[index] = _peakCandidatePrice;
+                    _seriesPeakPricesLow.Values[index] = _peakCandidatePrice;
+                }
+
+                _peakCandidateIndex = index;
+
+                if (betterUpPeakCandidateFound || betterDownPeakCandidateFound)
+                {
+                    ReCalcZigZagLinePointsForAllCandles(_seriesPeakPricesAll.Values, _seriesZigZagLinePointsOfAllCandles.Values);
+                }
+            }
+        }
+
+        private decimal GetExtremum(List<Candle> candles, int period, string pointType, int index)
+        {
+            try
+            {
+                List<decimal> values = new List<decimal>();
+                for (int i = index; i >= index - period; i--)
+                {
+                    values.Add(candles[i].GetPoint(pointType));
+                }
+                if (pointType == "High")
+                {
+                    return values.Max();
+                }
+                if (pointType == "Low")
+                {
+                    return values.Min();
+                }
+            }
+            catch { }
+
+            return 0;
+        }
+
+        private void ReCalcZigZagLinePointsForAllCandles(List<decimal> allPeaksPrices, List<decimal> zigZagLinePointsOfAllCandles)
+        {
+            decimal previousPeakPrice = 0;
+            int previousPeakPriceIndex = 0;
+
+            for (int currentPeakPriceIndex = 0; currentPeakPriceIndex < allPeaksPrices.Count; currentPeakPriceIndex++)
+            {
+                decimal currentPeakPrice = allPeaksPrices[currentPeakPriceIndex];
+                if (currentPeakPrice == 0)
+                {
+                    continue;
+                }
+
+                if (previousPeakPrice == 0)
+                {
+                    previousPeakPrice = currentPeakPrice;
+                    previousPeakPriceIndex = currentPeakPriceIndex;
+                    continue;
+                }
+
+                decimal peakToPeakLinePointPrice = previousPeakPrice;
+                bool goToDownPeak = currentPeakPrice < previousPeakPrice;
+                decimal priceDistanceBetweenLinePoints = Math.Abs(previousPeakPrice - currentPeakPrice) / (currentPeakPriceIndex - previousPeakPriceIndex);
+                for (int j = previousPeakPriceIndex; j < currentPeakPriceIndex; j++)
+                {
+                    zigZagLinePointsOfAllCandles[j] = peakToPeakLinePointPrice;
+                    peakToPeakLinePointPrice = goToDownPeak ? 
+                        peakToPeakLinePointPrice - priceDistanceBetweenLinePoints : 
+                        peakToPeakLinePointPrice + priceDistanceBetweenLinePoints;
+                }
+
+                previousPeakPriceIndex = currentPeakPriceIndex;
+                previousPeakPrice = currentPeakPrice;
+            }
+        }
+    }
+}
