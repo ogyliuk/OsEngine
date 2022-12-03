651bcdc2cb0f31149e1df29a0f225ef294c52836
Oleg Gyliuk <oleg_gyliuk@epam.com>
2022-12-03 16:10:51 +0100

    RecoveryZone : Added setting of next attempts entry orders at the moments of successful entries.

--- project/OsEngine/Robots/Oleg/Good/RecoveryZone.cs
+++ project/OsEngine/Robots/Oleg/Good/RecoveryZone.cs
@@ -12,9 +12,11 @@ namespace OsEngine.Robots.Oleg.Good
     [Bot("RecoveryZone")]
     public class RecoveryZone : BotPanel
     {
-        private BotTabSimple _tab;
+        private BotTabSimple _bot;
         private TradingState _state;
-        private decimal _balanceOnDealStart;
+        private decimal _zoneUp;
+        private decimal _zoneDown;
+        private decimal _balanceOnStart;
 
         private MovingAverage _bollingerSma;
         private Bollinger _bollinger;
@@ -34,50 +36,54 @@ namespace OsEngine.Robots.Oleg.Good
         public RecoveryZone(string name, StartProgram startProgram) : base(name, startProgram)
         {
             TabCreate(BotTabType.Simple);
-            _tab = TabsSimple[0];
-            _balanceOnDealStart = 0;
+            _bot = TabsSimple[0];
             _state = TradingState.FREE;
 
-            Regime = CreateParameter("Regime", "Off", new[] { "Off", "On", "OnlyLong", "OnlyShort", "OnlyClosePosition" }, "Base");
+            Regime = CreateParameter("Regime", "Off", new[] { "Off", "On" }, "Base");
             VolumeDecimals = CreateParameter("Decimals in Volume", 0, 0, 4, 1, "Base");
             VolumeMultiplier = CreateParameter("Volume multiplier", 0.75m, 0.25m, 1, 0.05m, "Base");
             MinVolumeUSDT = CreateParameter("Min Volume USDT", 7m, 7m, 7m, 1m, "Base");
-
             BollingerLength = CreateParameter("Length BOLLINGER", 20, 10, 50, 2, "Robot parameters");
             BollingerDeviation = CreateParameter("Bollinger deviation", 2m, 1m, 3m, 0.1m, "Robot parameters");
             BollingerSqueezeLength = CreateParameter("Length BOLLINGER SQUEEZE", 130, 100, 600, 5, "Robot parameters");
-
             ProfitSizeFromRZ = CreateParameter("Profit size from RZ", 2m, 0.5m, 3, 0.5m, "Base");
 
             _bollingerSma = new MovingAverage(false);
-            _bollingerSma = (MovingAverage)_tab.CreateCandleIndicator(_bollingerSma, "Prime");
+            _bollingerSma = (MovingAverage)_bot.CreateCandleIndicator(_bollingerSma, "Prime");
             _bollingerSma.TypeCalculationAverage = MovingAverageTypeCalculation.Simple;
             _bollingerSma.Lenght = BollingerLength.ValueInt;
             _bollingerSma.Save();
 
             _bollinger = new Bollinger(name + "Bollinger", false);
-            _bollinger = (Bollinger)_tab.CreateCandleIndicator(_bollinger, "Prime");
+            _bollinger = (Bollinger)_bot.CreateCandleIndicator(_bollinger, "Prime");
             _bollinger.Lenght = BollingerLength.ValueInt;
             _bollinger.Deviation = BollingerDeviation.ValueDecimal;
             _bollinger.Save();
 
             _bollingerWithSqueeze = new BollingerWithSqueeze(name + "BollingerWithSqueeze", false);
-            _bollingerWithSqueeze = (BollingerWithSqueeze)_tab.CreateCandleIndicator(_bollingerWithSqueeze, "Prime");
+            _bollingerWithSqueeze = (BollingerWithSqueeze)_bot.CreateCandleIndicator(_bollingerWithSqueeze, "Prime");
             _bollingerWithSqueeze.Lenght = BollingerLength.ValueInt;
             _bollingerWithSqueeze.Deviation = BollingerDeviation.ValueDecimal;
             _bollingerWithSqueeze.SqueezePeriod = BollingerSqueezeLength.ValueInt;
             _bollingerWithSqueeze.Save();
 
-            _tab.CandleFinishedEvent += _tab_CandleFinishedEventHandler_SQUEEZE_FOUND;
-            _tab.CandleUpdateEvent += _tab_CandleUpdateEventHandler_SET_NEXT_ORDERS;
-            _tab.PositionOpeningSuccesEvent += _tab_PositionOpenEventHandler_FILLED_ENTRY_ORDER;
-            _tab.PositionClosingSuccesEvent += _tab_PositionCloseEventHandler_FINISH_DEAL;
+            _bot.CandleFinishedEvent += event_CandleFinished_SQUEEZE_FOUND;
+            _bot.CandleUpdateEvent += event_CandleUpdated_SET_NEXT_ORDERS;
+            _bot.PositionOpeningSuccesEvent += event_PositionOpened_MANAGE_ENTRIES;
+            _bot.PositionClosingSuccesEvent += event_PositionClosed_FINISH_DEAL;
 
-            ParametrsChangeByUser += ParametersChangeByUserEventHandler;
-            ParametersChangeByUserEventHandler();
+            this.ParametrsChangeByUser += event_ParametersChangedByUser;
+            event_ParametersChangedByUser();
         }
 
-        private void ParametersChangeByUserEventHandler()
+        public override string GetNameStrategyType()
+        {
+            return "RecoveryZone";
+        }
+
+        public override void ShowIndividualSettingsDialog() { }
+
+        private void event_ParametersChangedByUser()
         {
             if (_bollingerSma.Lenght != BollingerLength.ValueInt)
             {
@@ -86,7 +92,7 @@ namespace OsEngine.Robots.Oleg.Good
                 _bollingerSma.Save();
             }
 
-            if (_bollinger.Lenght != BollingerLength.ValueInt || 
+            if (_bollinger.Lenght != BollingerLength.ValueInt ||
                 _bollinger.Deviation != BollingerDeviation.ValueDecimal)
             {
                 _bollinger.Lenght = BollingerLength.ValueInt;
@@ -107,112 +113,89 @@ namespace OsEngine.Robots.Oleg.Good
             }
         }
 
-        public override string GetNameStrategyType()
+        private void event_CandleFinished_SQUEEZE_FOUND(List<Candle> candles)
         {
-            return "RecoveryZone";
-        }
-
-        public override void ShowIndividualSettingsDialog() { }
-
-        private void _tab_CandleFinishedEventHandler_SQUEEZE_FOUND(List<Candle> candles)
-        {
-            if (IsBotEnabled())
+            if (ReadyToTrade())
             {
-                bool noPositions = _tab.PositionsOpenAll.Count == 0;
+                bool noPositions = _bot.PositionsOpenAll.Count == 0;
                 bool freeState = _state == TradingState.FREE;
                 bool lastCandleHasSqueeze = _bollingerWithSqueeze.ValuesSqueezeFlag.Last() > 0;
                 if (noPositions && freeState && lastCandleHasSqueeze)
                 {
                     _state = TradingState.SQUEEZE_FOUND;
-                    _balanceOnDealStart = _tab.Portfolio.ValueCurrent;
+                    _balanceOnStart = _bot.Portfolio.ValueCurrent;
 
-                    decimal semiSqueezeVolatility = (_bollingerWithSqueeze.ValuesUp.Last() - _bollingerWithSqueeze.ValuesDown.Last()) / 2;
-
-                    bool longsEnabled = Regime.ValueString == "On" || Regime.ValueString == "OnlyLong";
-                    if (longsEnabled)
-                    {
-                        decimal buyCoinsVolume = GetNewAttemptCoinsVolume(Side.Buy);
-                        if (buyCoinsVolume > 0)
-                        {
-                            decimal longEntryPrice = _bollingerWithSqueeze.ValuesUp.Last() + semiSqueezeVolatility;
-                            _tab.BuyAtStop(buyCoinsVolume, longEntryPrice, longEntryPrice, StopActivateType.HigherOrEqual, 100);
-                        }
-                    }
-
-                    bool shortsEnabled = Regime.ValueString == "On" || Regime.ValueString == "OnlyShort";
-                    if (shortsEnabled)
-                    {
-                        decimal sellCoinsVolume = GetNewAttemptCoinsVolume(Side.Sell);
-                        if (sellCoinsVolume > 0)
-                        {
-                            decimal shortEntryPrice = _bollingerWithSqueeze.ValuesDown.Last() - semiSqueezeVolatility;
-                            _tab.SellAtStop(sellCoinsVolume, shortEntryPrice, shortEntryPrice, StopActivateType.LowerOrEqyal, 100);
-                        }
-                    }
+                    decimal squeezeSize = _bollingerWithSqueeze.ValuesUp.Last() - _bollingerWithSqueeze.ValuesDown.Last();
+                    _zoneUp = _bollingerWithSqueeze.ValuesUp.Last() + squeezeSize / 2;
+                    _zoneDown = _bollingerWithSqueeze.ValuesDown.Last() - squeezeSize / 2;
+                    SetNewEntryOrder_LONG();
+                    SetNewEntryOrder_SHORT();
                 }
             }
         }
 
-        private void _tab_PositionOpenEventHandler_FILLED_ENTRY_ORDER(Position p)
+        private void event_PositionOpened_MANAGE_ENTRIES(Position p)
         {
             if (p != null && p.State == PositionStateType.Open)
             {
-                _tab.SellAtStopCancel();
-                _tab.BuyAtStopCancel();
-
                 if (p.Direction == Side.Buy)
                 {
                     _state = TradingState.LONG_ENTERED;
+                    _zoneDown = _bot.PositionsOpenAll.Count == 1 ? _bollingerSma.Values.Last() : _zoneDown;
+                    SetNewEntryOrder_SHORT();
                 }
+
                 if (p.Direction == Side.Sell)
                 {
                     _state = TradingState.SHORT_ENTERED;
+                    _zoneUp = _bot.PositionsOpenAll.Count == 1 ? _bollingerSma.Values.Last() : _zoneUp;
+                    SetNewEntryOrder_LONG();
                 }
             }
         }
 
-        private void _tab_CandleUpdateEventHandler_SET_NEXT_ORDERS(List<Candle> candles)
+        private void event_CandleUpdated_SET_NEXT_ORDERS(List<Candle> candles)
         {
-            if (IsBotEnabled() && _tab.PositionsOpenAll.Count > 0)
+            if (_bot.PositionsOpenAll.Count > 0)
             {
                 if (_state == TradingState.LONG_ENTERED)
                 {
-                    Position longPosition = _tab.PositionsOpenAll
+                    Position longPosition = _bot.PositionsOpenAll
                         .Where(p => p.State == PositionStateType.Open && p.Direction == Side.Buy).FirstOrDefault();
                     if (longPosition != null)
                     {
-                        decimal SL_price = _bollingerSma.Values.Last();
+                        decimal SL_price = _zoneDown;
                         decimal SL_size = longPosition.EntryPrice - SL_price;
                         decimal TP_price = longPosition.EntryPrice + SL_size * ProfitSizeFromRZ.ValueDecimal;
-                        _tab.CloseAtProfit(longPosition, TP_price, longPosition.OpenVolume);
-                        _tab.CloseAtStop(longPosition, SL_price, SL_price);
+                        _bot.CloseAtProfit(longPosition, TP_price, longPosition.OpenVolume);
+                        _bot.CloseAtStop(longPosition, SL_price, SL_price);
                         _state = TradingState.LONG_TARGETS_SET;
                     }
                 }
 
                 if (_state == TradingState.SHORT_ENTERED)
                 {
-                    Position shortPosition = _tab.PositionsOpenAll
+                    Position shortPosition = _bot.PositionsOpenAll
                         .Where(p => p.State == PositionStateType.Open && p.Direction == Side.Sell).FirstOrDefault();
                     if (shortPosition != null)
                     {
-                        decimal SL_price = _bollingerSma.Values.Last();
+                        decimal SL_price = _zoneUp;
                         decimal SL_size = SL_price - shortPosition.EntryPrice;
                         decimal TP_price = shortPosition.EntryPrice - SL_size * ProfitSizeFromRZ.ValueDecimal;
-                        _tab.CloseAtProfit(shortPosition, TP_price, shortPosition.OpenVolume);
-                        _tab.CloseAtStop(shortPosition, SL_price, SL_price);
+                        _bot.CloseAtProfit(shortPosition, TP_price, shortPosition.OpenVolume);
+                        _bot.CloseAtStop(shortPosition, SL_price, SL_price);
                         _state = TradingState.SHORT_TARGETS_SET;
                     }
                 }
             }
         }
 
-        private void _tab_PositionCloseEventHandler_FINISH_DEAL(Position p)
+        private void event_PositionClosed_FINISH_DEAL(Position p)
         {
             if (p != null && p.State == PositionStateType.Done)
             {
-                _balanceOnDealStart = 0;
                 _state = TradingState.FREE;
+
                 OlegUtils.Log("\n#{0} {1}\n\tvolume = {2}\n\topen price = {3}\n\tclose price = {4}\n\tfee = {5}% = {6}$" + 
                     "\n\tprice change = {7}% = {8}$\n\tdepo profit = {9}% = {10}$\n\tDEPO: {11}$ ===> {12}$", 
                     p.Number,
@@ -232,16 +215,33 @@ namespace OsEngine.Robots.Oleg.Good
             }
         }
 
-        private bool IsBotEnabled()
+        private void SetNewEntryOrder_LONG()
         {
-            if (Regime.ValueString == "Off" ||
-                _tab.CandlesAll == null ||
-                BollingerLength.ValueInt >= _tab.CandlesAll.Count ||
-                BollingerSqueezeLength.ValueInt >= _tab.CandlesAll.Count)
+            _bot.BuyAtStopCancel();
+            decimal buyCoinsVolume = GetNewAttemptCoinsVolume(Side.Buy);
+            if (buyCoinsVolume > 0)
             {
-                return false;
+                _bot.BuyAtStop(buyCoinsVolume, _zoneUp, _zoneUp, StopActivateType.HigherOrEqual, 100);
             }
-            return true;
+        }
+
+        private void SetNewEntryOrder_SHORT()
+        {
+            _bot.SellAtStopCancel();
+            decimal sellCoinsVolume = GetNewAttemptCoinsVolume(Side.Sell);
+            if (sellCoinsVolume > 0)
+            {
+                _bot.SellAtStop(sellCoinsVolume, _zoneDown, _zoneDown, StopActivateType.LowerOrEqyal, 100);
+            }
+        }
+
+        private bool ReadyToTrade()
+        {
+            int candlesCount = _bot.CandlesAll != null ? _bot.CandlesAll.Count : 0;
+            bool robotEnabled = Regime.ValueString == "On";
+            bool enoughCandlesForBollinger = candlesCount > BollingerLength.ValueInt;
+            bool enoughCandlesForBollingerSqueeze = candlesCount > BollingerSqueezeLength.ValueInt;
+            return robotEnabled && enoughCandlesForBollinger && enoughCandlesForBollingerSqueeze;
         }
 
         private decimal GetNewAttemptCoinsVolume(Side side)
@@ -250,7 +250,7 @@ namespace OsEngine.Robots.Oleg.Good
             if (HasMoneyForNewAttempt() && !IsMoneyForNewAttemptTooSmall())
             {
                 decimal moneyAvailableForNewAttempt = GetNewAttemptMoneyNeeded();
-                decimal moneyNeededForFee = moneyAvailableForNewAttempt / 100 * _tab.ComissionValue;
+                decimal moneyNeededForFee = moneyAvailableForNewAttempt / 100 * _bot.ComissionValue;
                 decimal moneyLeftForCoins = moneyAvailableForNewAttempt - moneyNeededForFee;
                 decimal price = side == Side.Buy ? TabsSimple[0].PriceBestAsk : TabsSimple[0].PriceBestBid;
                 decimal volume = moneyLeftForCoins / price;
@@ -271,20 +271,20 @@ namespace OsEngine.Robots.Oleg.Good
 
         private decimal GetNewAttemptMoneyNeeded()
         {
-            bool hasOpenPositionsAlready = _tab.PositionsLast != null && _tab.PositionsLast.State == PositionStateType.Open;
-            decimal moneyToCalcFrom = hasOpenPositionsAlready ? GetLastPositionEntryMoney() : _tab.Portfolio.ValueCurrent;
+            bool hasOpenPositionsAlready = _bot.PositionsLast != null && _bot.PositionsLast.State == PositionStateType.Open;
+            decimal moneyToCalcFrom = hasOpenPositionsAlready ? GetLastPositionEntryMoney() : _bot.Portfolio.ValueCurrent;
             return moneyToCalcFrom * VolumeMultiplier.ValueDecimal;
         }
 
         private decimal GetLastPositionEntryMoney()
         {
-            return _tab.PositionsLast != null ? GetPositionEntryMoney(_tab.PositionsLast) : 0;
+            return _bot.PositionsLast != null ? GetPositionEntryMoney(_bot.PositionsLast) : 0;
         }
 
         private decimal GetFreeMoney()
         {
-            decimal freeMoney = _balanceOnDealStart;
-            _tab.PositionsOpenAll.ForEach(p => { freeMoney -= GetPositionEntryMoney(p); });
+            decimal freeMoney = _balanceOnStart;
+            _bot.PositionsOpenAll.ForEach(p => { freeMoney -= GetPositionEntryMoney(p); });
             return freeMoney;
         }
 
